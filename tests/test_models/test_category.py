81235627bdac93b5feb1b5e8ea2612cdffcba9be
Alex <Alex@DESKTOP-4SHP6LQ>
2024-03-10 13:04:06 +0300

    change path commit

--- tests/test_models/test_category.py
+++ tests/test_models/test_category.py
@@ -4,9 +4,8 @@
 from inspect import isgenerator
 
 import pytest
-
-from models.category import Category
-from  bookkeeper.repository.memory_repository import MemoryRepository
+from bookkeeper.bookkeeper.models.category import Category
+from bookkeeper.bookkeeper.repository.memory_repository import MemoryRepository
 
 
 @pytest.fixture
